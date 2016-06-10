a2be350bfb795d507f42bc33c3935a2b1a9b93d8
Helga13 <oliatrubina@yandex.ru>
2016-06-10 12:47:58 +0300

    correct path

--- mebel/news/rss.php
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?><rss version="2.0">
-<channel>
-<title>Новости</title>
-<link>http://alarm.flxmd.by</link>
-<description></description>
-<lastBuildDate>Mon, 11 Apr 2016 14:06:06 +0300</lastBuildDate>
-<ttl>60</ttl>
-</channel>
-</rss>
