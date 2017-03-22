9a1bc9b39d87303fbac2f08e6f82b4946f9d3196
gitwhatislove <whatislove0505@gmail.com>
2017-03-22 16:11:19 +0300

    'sorry_old_bug_fix'

--- bitrix/js/custom.js
+++ bitrix/js/custom.js
@@ -24,7 +24,15 @@ b.params.hashnav&&b.hashnav&&b.hashnav.init(),b.params.a11y&&b.a11y&&b.a11y.init
 // end swiper.min.js
 
 $(document).ready(function () {
-	
+	// new_v
+	$('.multiple-items').slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		prevArrow: "<button type='button' class='slider_arrow slider_arrow-pre'><svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='0 0 15 27' enable-background='new 0 0 15 27' xml:space='preserve'><path fill-rule='evenodd' clip-rule='evenodd' d='M13.8,0L15,1.1L2.7,13.6L15,25.7L13.7,27L0,13.7L13.8,0z'/></svg></button>",
+		nextArrow: "<button type='button' class='slider_arrow slider_arrow-next'><svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='0 0 15 27' enable-background='new 0 0 15 27' xml:space='preserve'><path fill-rule='evenodd' clip-rule='evenodd' d='M15,13.7L1.3,27L0,25.7l12.3-12.1L0,1.1L1.2,0L15,13.7z'/></svg></button>"
+	});
+	// new_v
 	var swiper = new Swiper('.swiper-container-main', {
         nextButton: '.swiper-button-next',
         prevButton: '.swiper-button-prev',
